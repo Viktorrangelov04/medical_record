3ea311c98700b5438e7df4ba135163ff0481ddca
Viktor Rangelov <viktorrangelov@MacBook-Air.local>
2026-06-28 15:08:44 +0300

    adding exceptions and exception handler

---
+++ src/main/java/com/example/cscb869_medicalrecord/exception/ValidationException.java
@@ -0,0 +1,7 @@
+package com.example.cscb869_medicalrecord.exception;
+
+public class ValidationException extends RuntimeException {
+    public ValidationException(String message) {
+        super(message);
+    }
+}
